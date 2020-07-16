f879a43acd9b8b3bf643874827ff9aa04f46cad1
Colin787 <colinpark4@gmail.com>
2020-07-16 06:48:06 -0700

    Could not figure out how to seperate models.py file and link correctly

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 #Importing Flask so that I can use the helpful framework
 from flask import Flask, render_template
-#Importing Flack SQLAlchemy so I can use these commands in code ex. db.session.X
-from flask_sqlalchemy import SQLAlchemy
-
+#Helper for migrating in flask command line style
+from flask_migrate import Migrate
 from models import *
+from config import Config
+
+
+APPLICATION_NAME = "app.py"
 
 #assigning the name of my app to "app" so as to run the app via this variable
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app) 
+migrate = Migrate(app, db)
 
 # Set the Config file 
-app.config.from_object('config')
+
+class Todo(db.Model):
+	__tablename__ = 'todos'
+	id = db.Column(db.Integer, primary_key=True)
+	description = db.Column(db.String(), nullable=False)
+	completed = db.Column(db.Boolean, default=False)
+
 
 @app.route('/')
 def index():
-	return render_template('index.html', data=[{
-		'description': 'Stuff'
-	}, {
-		'description': 'Things'
-	}, {
-		'description': 'WOW Take my Accounts'
-	}])
+	return render_template('index.html', data=Todo.query.order_by('id').all())
+
+
+
+
--- config.py
+++ config.py
@@ -1,13 +1,21 @@
 import os
-SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
-DEBUG = True
 
-# Connect to the database
+class Config(object):
+    """Set Flask configuration vars."""
 
+    DEBUG = True
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:peaches777@127.0.0.1:5432/Todo'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:peaches777@127.0.0.1:5432/TodoV2'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = os.urandom(32)
+
+    SESSION_COOKIE_SECURE = True
+
+    SESSION_COOKIE_NAME = 'Todo-App-Cookie'
+
+    WTF_CSRF_TIME_LIMIT = None
